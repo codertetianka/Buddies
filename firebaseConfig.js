a47bcf89f479df8bcd97d97f61ca86253e3a6c22
grace-hartley <gracerita.hartley@gmail.com>
2024-02-27 09:23:50 +0000

    removed firebase details

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,22 +9,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 
-// const firebaseConfig = {
-//   apiKey: process.env.EXPO_PUBLIC_API_KEY,
-//   authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-//   projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-//   storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
-//   messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-//   appId: process.env.EXPO_PUBLIC_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyC5DI_bgGV75V8PCA1nB7D7aQ4u72DieoA",
-  authDomain: "buddies-82f2e.firebaseapp.com",
-  projectId: "buddies-82f2e",
-  storageBucket: "buddies-82f2e.appspot.com",
-  messagingSenderId: "180350395573",
-  appId: "1:180350395573:web:dbeeeab601d113fb1acafc",
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
